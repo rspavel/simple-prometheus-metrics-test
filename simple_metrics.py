d06f2844e6b5f307fb6d485b772b0eaa2a8226e2
Robert Pavel <robert@rspavel.net>
2024-09-04 13:15:58 -0600

    Initial Version of Metrics

---
+++ simple_metrics.py
@@ -0,0 +1,14 @@
+from prometheus_client import start_http_server, Counter
+import random
+import time
+
+# Heavily based on https://prometheus.github.io/client_python/getting-started/three-step-demo/
+
+if __name__ == '__main__':
+    start_http_server(8000)
+    cnt = Counter('simple_count', 'A Mostly Pointless Metric')
+    while True:
+        time.sleep(random.random())
+        cnt.inc(1.0)
+
+
